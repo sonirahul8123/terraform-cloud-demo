d2f25fdabe895f945e8bd267f406aac5fa1c561a
Rahul Soni <soni.rahul073@gmail.com>
2022-08-29 11:44:45 +0530

    Update gce.tf

--- gce.tf
+++ gce.tf
@@ -10,4 +10,12 @@ resource "google_compute_instance" "default" {
       image = "debian-cloud/debian-11"
     }
   }
+  
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
 }
